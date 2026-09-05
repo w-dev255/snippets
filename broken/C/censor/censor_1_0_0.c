ed664784d1c9ad50eaf930a3043ce4a36e127da8
w-dev255 <devangleteam@gmail.com>
2026-09-05 18:22:53 +0200

    update

--- broken/C/censor/censor_1_0_0.c
+++ broken/C/censor/censor_1_0_0.c
@@ -1,1 +1,21 @@
-/*To rewrite*/
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#include <unistd.h>
+
+#include <fcntl.h>
+
+typedef struct {
+	char* input_buffer;
+	int input_buffer_len;
+
+	char* output_buffer;
+	int output_buffer_len;
+
+	char* word;
+	int word_len;
+} DATA;
+
