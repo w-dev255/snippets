f78829cebacdc817638b5a173f8e54e3eab12f9b
w-dev255 <devangleteam@gmail.com>
2026-09-05 18:34:02 +0200

    update

--- broken/C/censor/censor_1_0_0.c
+++ broken/C/censor/censor_1_0_0.c
@@ -12,12 +12,24 @@ typedef struct {
 	char* input_buffer;
 	int input_buffer_len;
 	char* input_path;
+	int err;
+} INPUT;
 
+typedef struct {
 	char* output_buffer;
 	int output_buffer_len;
 	char* output_path;
+	int err;
+} OUTPUT;
 
+typedef struct {
 	char* word;
 	int word_len;
+} WORD;
+
+typedef struct {
+	INPUT* input;
+	OUTPUT* output;
+	WORD* word;
 } DATA;
 
